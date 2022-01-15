2bb5fde17bee3e1b7097bbdfe25a50b419ca7e68
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-01-15 20:41:37 +0000

    Prettier

--- src/state.ts
+++ src/state.ts
@@ -1,9 +1,9 @@
-import * as fen from './fen';
-import { AnimCurrent } from './anim';
-import { DragCurrent } from './drag';
-import { Drawable } from './draw';
-import { timer } from './util';
-import * as cg from './types';
+import * as fen from "./fen";
+import { AnimCurrent } from "./anim";
+import { DragCurrent } from "./drag";
+import { Drawable } from "./draw";
+import { timer } from "./util";
+import * as cg from "./types";
 
 export interface HeadlessState {
   pieces: cg.Pieces;
@@ -32,7 +32,7 @@ export interface HeadlessState {
   };
   movable: {
     free: boolean;
-    color?: cg.Color | 'both';
+    color?: cg.Color | "both";
     dests?: cg.Dests;
     showDests: boolean;
     events: {
@@ -101,10 +101,10 @@ export interface State extends HeadlessState {
 export function defaults(): HeadlessState {
   return {
     pieces: fen.read(fen.initial),
-    orientation: 'white',
-    turnColor: 'white',
+    orientation: "white",
+    turnColor: "white",
     coordinates: true,
-    ranksPosition: 'right',
+    ranksPosition: "right",
     autoCastle: true,
     viewOnly: false,
     disableContextMenu: false,
@@ -122,7 +122,7 @@ export function defaults(): HeadlessState {
     },
     movable: {
       free: true,
-      color: 'both',
+      color: "both",
       showDests: true,
       events: {},
       rookCastle: true,
@@ -151,7 +151,7 @@ export function defaults(): HeadlessState {
       enabled: true,
     },
     stats: {
-      dragged: !('ontouchstart' in window),
+      dragged: !("ontouchstart" in window),
     },
     events: {},
     drawable: {
@@ -162,24 +162,24 @@ export function defaults(): HeadlessState {
       shapes: [],
       autoShapes: [],
       brushes: {
-        green: { key: 'g', color: '#15781B', opacity: 1, lineWidth: 10 },
-        red: { key: 'r', color: '#882020', opacity: 1, lineWidth: 10 },
-        blue: { key: 'b', color: '#003088', opacity: 1, lineWidth: 10 },
-        yellow: { key: 'y', color: '#e68f00', opacity: 1, lineWidth: 10 },
-        paleBlue: { key: 'pb', color: '#003088', opacity: 0.4, lineWidth: 15 },
-        paleGreen: { key: 'pg', color: '#15781B', opacity: 0.4, lineWidth: 15 },
-        paleRed: { key: 'pr', color: '#882020', opacity: 0.4, lineWidth: 15 },
+        green: { key: "g", color: "#15781B", opacity: 1, lineWidth: 10 },
+        red: { key: "r", color: "#882020", opacity: 1, lineWidth: 10 },
+        blue: { key: "b", color: "#003088", opacity: 1, lineWidth: 10 },
+        yellow: { key: "y", color: "#e68f00", opacity: 1, lineWidth: 10 },
+        paleBlue: { key: "pb", color: "#003088", opacity: 0.4, lineWidth: 15 },
+        paleGreen: { key: "pg", color: "#15781B", opacity: 0.4, lineWidth: 15 },
+        paleRed: { key: "pr", color: "#882020", opacity: 0.4, lineWidth: 15 },
         paleGrey: {
-          key: 'pgr',
-          color: '#4a4a4a',
+          key: "pgr",
+          color: "#4a4a4a",
           opacity: 0.35,
           lineWidth: 15,
         },
       },
       pieces: {
-        baseUrl: 'https://lichess1.org/assets/piece/cburnett/',
+        baseUrl: "https://lichess1.org/assets/piece/cburnett/",
       },
-      prevSvgHash: '',
+      prevSvgHash: "",
     },
     hold: timer(),
   };
